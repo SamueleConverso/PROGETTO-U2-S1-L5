b988a782675967842bd4c99c07ad6d627b4c76c2
Samuele Converso <samu.converso@gmail.com>
2024-11-29 17:52:20 +0100

    .

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,8 @@
 const header = document.getElementById("header");
 const btnLi = document.getElementById("btnLi");
 // const htmlElem = document.querySelector("[aria-label]");
-const gElems = document.querySelectorAll("svg g:not(:firstChild)");
+//const gElems = document.querySelectorAll("g");
 // const words = htmlElem.getAttribute("aria-label");
-const words = gElems.getAttribute("aria-label");
 // const mChars = `M M M M M M M M M M M M M M
 //             M M M M M M M M M M M M M M M M M M M M M
 //            M M M M M M M M M M M M M M M M M M M M M M M M M
@@ -21,18 +20,17 @@ const words = gElems.getAttribute("aria-label");
 //             M M M M M M M M M M M M M M M M M
 //             M M M M M M M M M M M M M M M M`;
 
-setInterval(function () {
-  let newWords = words;
-  let ranNum = Math.floor(Math.random() * newWords.length);
-  newWords[ranNum].replace("M", "");
-  gElem.setAttribute("aria-label", "ciao");
-}, 1000);
+// setInterval(function () {
+//   let newWords = words;
+//   let ranNum = Math.floor(Math.random() * newWords.length);
+//   newWords[ranNum].replace("M", "");
+//   gElems.setAttribute("aria-label", "ciao");
+// }, 1000);
 
 window.addEventListener("scroll", function () {
   if (window.scrollY > 250) {
     header.style.backgroundColor = "white";
     btnLi.style.backgroundColor = "#1a8917";
-    console.log(words);
   } else {
     header.style.backgroundColor = "#ffc017";
     btnLi.style.backgroundColor = "black";
