1871904b570d67eaf0dc387219609c472a12791d
Samuele Converso <samu.converso@gmail.com>
2024-11-29 16:14:09 +0100

    .

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,12 @@
 const header = document.getElementById("header");
+const btnLi = document.getElementById("btnLi");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 300) {
+    header.style.backgroundColor = "white";
+    btnLi.style.backgroundColor = "green";
+  } else {
+    header.style.backgroundColor = "#ffc017";
+    btnLi.style.backgroundColor = "black";
+  }
+});
