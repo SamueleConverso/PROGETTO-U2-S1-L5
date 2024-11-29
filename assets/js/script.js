af5f9a193c943751aa94f39df36cf8ad8f384474
Samuele Converso <samu.converso@gmail.com>
2024-11-29 17:28:27 +0100

    .

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,33 @@
 const header = document.getElementById("header");
 const btnLi = document.getElementById("btnLi");
-const htmlElem = document.querySelector("[aria-label]");
-const words = htmlElem.getAttribute("aria-label");
-const mChars = `M M M M M M M M M M M M M M
-            M M M M M M M M M M M M M M M M M M M M M
-           M M M M M M M M M M M M M M M M M M M M M M M M M
-            M M M M M M M M M M M M
-            M M M M M M M M M M M M M M M M M M M
-            M M M M M M M M M M M M M M M M M M M M M
-            M M M M M M M M M M M M M M
-           M M M M M M M M M M M M M M M M M
-           M M M M M M M M M M M M M M M M M M M
-            M M M M M M M M M M M M M M M M M M M M M M M
-           M M M M M M M M M M M M
-            M M M M M M M M M M M M
-            M M M M M M M M M M M M M M M M M M M M M
-            M M M M M M M M M M M
-            M M M M M M M M M M M M M M M M M
-            M M M M M M M M M M M M M M M M`;
+// const htmlElem = document.querySelector("[aria-label]");
+const gElems = document.querySelectorAll("svg g:not(:firstChild)");
+// const words = htmlElem.getAttribute("aria-label");
+const words = gElems.getAttribute("aria-label");
+// const mChars = `M M M M M M M M M M M M M M
+//             M M M M M M M M M M M M M M M M M M M M M
+//            M M M M M M M M M M M M M M M M M M M M M M M M M
+//             M M M M M M M M M M M M
+//             M M M M M M M M M M M M M M M M M M M
+//             M M M M M M M M M M M M M M M M M M M M M
+//             M M M M M M M M M M M M M M
+//            M M M M M M M M M M M M M M M M M
+//            M M M M M M M M M M M M M M M M M M M
+//             M M M M M M M M M M M M M M M M M M M M M M M
+//            M M M M M M M M M M M M
+//             M M M M M M M M M M M M
+//             M M M M M M M M M M M M M M M M M M M M M
+//             M M M M M M M M M M M
+//             M M M M M M M M M M M M M M M M M
+//             M M M M M M M M M M M M M M M M`;
+
+setInterval(function () {
+  let newWords = words;
+  let ranNum = Math.floor(Math.random() * newWords.length);
+  newWords[ranNum].replace("M", "");
+  gElem.setAttribute("aria-label", "ciao");
+}, 1000);
+
 window.addEventListener("scroll", function () {
   if (window.scrollY > 250) {
     header.style.backgroundColor = "white";
