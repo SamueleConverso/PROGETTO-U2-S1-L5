9b481d82f3c312aa72f001f4ce7152c89821517f
Samuele Converso <samu.converso@gmail.com>
2024-11-29 16:43:12 +0100

    .

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,28 @@
 const header = document.getElementById("header");
 const btnLi = document.getElementById("btnLi");
-
+const htmlElem = document.querySelector("[aria-label]");
+const words = htmlElem.getAttribute("aria-label");
+const mChars = `M M M M M M M M M M M M M M
+            M M M M M M M M M M M M M M M M M M M M M
+           M M M M M M M M M M M M M M M M M M M M M M M M M
+            M M M M M M M M M M M M
+            M M M M M M M M M M M M M M M M M M M
+            M M M M M M M M M M M M M M M M M M M M M
+            M M M M M M M M M M M M M M
+           M M M M M M M M M M M M M M M M M
+           M M M M M M M M M M M M M M M M M M M
+            M M M M M M M M M M M M M M M M M M M M M M M
+           M M M M M M M M M M M M
+            M M M M M M M M M M M M
+            M M M M M M M M M M M M M M M M M M M M M
+            M M M M M M M M M M M
+            M M M M M M M M M M M M M M M M M
+            M M M M M M M M M M M M M M M M`;
 window.addEventListener("scroll", function () {
-  if (window.scrollY > 300) {
+  if (window.scrollY > 250) {
     header.style.backgroundColor = "white";
-    btnLi.style.backgroundColor = "green";
+    btnLi.style.backgroundColor = "#1a8917";
+    console.log(words);
   } else {
     header.style.backgroundColor = "#ffc017";
     btnLi.style.backgroundColor = "black";
